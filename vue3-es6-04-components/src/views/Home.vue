1f0769739600be2e5ecb121b5faf811470f1b1cc
mingyuchoo <mingyuchoo@gmail.com>
2021-06-17 18:51:36 +0900

    t570

---
+++ vue3-es6-04-components/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <h2>Home</h2>
+    <p id="poem">
+      Vivamus, mea Lesbia, atque amemus, rumoresque senum severiorum omnes unius
+      aestimemus assis! soles occidere et redire possunt: nobis cum semel
+      occidit brevis lux, nox est perpetua una dormienda. da mi basia mille,
+      deinde centum, dein mille altera, dein secunda centum, deinde usque altera
+      mille, deinde centum. dein, cum milia multa fecerimus, conturbabimus illa,
+      ne sciamus, aut ne quis malus invidere possit, cum tantum sciat esse
+      basiorum.
+    </p>
+  </div>
+</template>
+
+<style>
+p#poem {
+  text-align: left;
+  padding: 10px 20px;
+}
+</style>
