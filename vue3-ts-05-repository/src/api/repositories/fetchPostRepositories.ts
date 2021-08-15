0d38ff05ad1421be268303d02ff91ac4a2098368
my-name <my-email-address>
2021-08-15 20:16:27 +0900

    t570

---
+++ vue3-ts-05-repository/src/api/repositories/fetchPostRepositories.ts
@@ -0,0 +1,46 @@
+import { useAxios } from "@vue-composable/axios";
+import { AxiosRequestConfig } from "axios";
+import { ComputedRef, Ref } from "vue";
+
+// user type declaration
+export type Post = {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+};
+
+// function return type declaration
+export type FetchPostRepositoriesRet = Promise<{
+  data: ComputedRef<Post[]>;
+  loading: Ref<boolean>;
+  error: Ref<any>;
+  status: Ref<number | null>;
+}>;
+
+// function type signature
+export type FetchPostRepositories = () => FetchPostRepositoriesRet;
+
+// function definition
+const fetchPostRepositories: FetchPostRepositories = async () => {
+  const HTTP_METHOD = "GET";
+  const SERVER_URL = "https://jsonplaceholder.typicode.com";
+  const SERVICE_PATH = "/posts";
+
+  const request: AxiosRequestConfig = {
+    method: HTTP_METHOD,
+    url: SERVER_URL + SERVICE_PATH,
+  };
+  const { exec, data, loading, error, status } = useAxios();
+
+  await exec(request);
+
+  return {
+    data,
+    loading,
+    error,
+    status,
+  };
+};
+
+export default fetchPostRepositories;
--- vue3-ts-05-repository/src/api/repositories/index.ts
+++ vue3-ts-05-repository/src/api/repositories/index.ts
@@ -1,1 +1,2 @@
+export * from "@/api/repositories/fetchPostRepositories";
 export * from "@/api/repositories/fetchUserRepositories";
