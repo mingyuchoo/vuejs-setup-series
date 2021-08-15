bd9e75d2ea9720fbcea0f9057250a92f5639361b
my-name <my-email-address>
2021-08-15 10:03:21 +0900

    added types

--- vue3-ts-05-repository/src/composables/useUserRepositories.ts
+++ vue3-ts-05-repository/src/composables/useUserRepositories.ts
@@ -1,7 +1,20 @@
 import fetchUserRepositories from "@/api/repositories/fetchUserRepositories";
-import { ref, onMounted } from "vue";
+import { ref, onMounted, Ref } from "vue";
 
-export const useUserRepositories = () => {
+// function type signature
+type GetUserRepositorie = () => void;
+
+// return type
+type UseUserRepositoriesRet = {
+  repositories: Ref<never[]>;
+  getUserRepositories: GetUserRepositorie;
+};
+
+// function type signature
+type UseUserRepositories = () => UseUserRepositoriesRet;
+
+// function definition
+export const useUserRepositories: UseUserRepositories = () => {
   const repositories = ref([]);
   const getUserRepositories = () => {
     repositories.value = fetchUserRepositories() as never[];
